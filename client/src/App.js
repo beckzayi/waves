f3ef02d024dd07719ec78070f75401676ad8b780
Darren Li <beckzayi@126.com>
2019-05-17 15:44:04 +1000

    Client start client | proxy in client, concurrently run server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    axios.get('/api/product/brands')
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        My App
+      </div>
+    );
+  }
 }
 
 export default App;
